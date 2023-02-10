7a172bbd3c51be9383438f5e3a91c4642b800583
ygorigor <sorocean.igor@gmail.com>
2023-02-10 12:20:06 +0200

    don't stop docker container

--- lizmap.dir/instances/docker_container_entrypoint.sh
+++ lizmap.dir/instances/docker_container_entrypoint.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-sleep 5 &&
 echo "Install lizmap plugins" &&
 docker exec map sh /srv/projects/install-lizmap-plugin.sh &&
 sleep 10 &&
 echo "Load LWC cache"
 docker exec lizmap php lizmap/scripts/script.php lizmap~project:load 5 &&
-sleep 2 &&
-echo "Stop docker container"
-docker stop docker_container
+sleep 7d
