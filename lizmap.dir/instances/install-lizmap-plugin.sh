8a408376db707849d229e9645497f27565893a39
Igor Sorocean <igor_sorocean@hotmail.com>
2023-06-06 11:09:47 +0300

    remove atlasprint plugin

--- lizmap.dir/instances/install-lizmap-plugin.sh
+++ lizmap.dir/instances/install-lizmap-plugin.sh
@@ -4,10 +4,9 @@
 # Install lizmap plugin 
 # This has to be run inside the qgis server container:
 #
-echo "Install QGIS Server, WfsOutputExtension and AlasPrint plugins"
+echo "Install QGIS Server and WfsOutputExtension"
 qgis-plugin-manager init
 qgis-plugin-manager update
 qgis-plugin-manager install "Lizmap server"
-qgis-plugin-manager install atlasprint
 qgis-plugin-manager install wfsOutputExtension
 touch /srv/projects/qgis-restart.txt
